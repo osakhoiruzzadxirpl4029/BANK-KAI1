c07d66dc0f33bf401a541de67d97d7c9828117fc
osakhoiruzzadxirpl4029 <khoiruzzad72@gmail.com>
2017-02-12 23:06:30 +0700

    Bruh

---
+++ src/bank/kai/setor.java
@@ -0,0 +1,23 @@
+
+package bank.kai;
+
+import java.*;
+import javax.swing.JOptionPane;
+
+public class setor {
+     private int saldo;
+    public void menabung (int tab, int s)
+    {
+        if(s%100!=0)
+            JOptionPane.showMessageDialog(null, "Mesin ATM BRI tidak menerima uang koin \n Silahkan Ulangi Kembali !", "Transaksi Gagal",0);
+        else if(s<50000)
+            JOptionPane.showMessageDialog(null, "Besaran minimal setor tunai adalah Rp.50.000,00", "ERROR",0);
+        else if(s>=50000)
+            saldo = tab+s;
+            JOptionPane.showMessageDialog(null, "Saldo anda saat ini sebesar : " + saldo);
+    }
+
+    public int getsaldo(){
+        return saldo;
+    }
+}
